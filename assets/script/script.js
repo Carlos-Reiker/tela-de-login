c96e923e074359584cb6a25b943b8b48f79e504b
Carlos Reiker <carlos.reiker@hotmail.com>
2022-12-04 20:13:32 -0300

    show/hide password button
    create a hide/show button. Visible of a icon eye

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+document.getElementById('eye').addEventListener('mousedown', function(){
+    document.getElementById('pass').type = 'text';
+});
+
+document.getElementById('eye').addEventListener('mouseup', function(){
+    document.getElementById('pass').type = 'password';
+});
+
+document.getElementById('eye').addEventListener('mousemove', function(){
+    document.getElementById('pass').type = 'password';
+});
